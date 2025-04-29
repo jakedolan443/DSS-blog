76f099b38de12307ea97ca51588d9e0ff37cd5d4
Jake Dolan <jakedolan443@pm.me>
2025-04-29 11:02:42 +0100

    added bcrypt seeding

--- seeds/seed_users.js
+++ seeds/seed_users.js
@@ -1,12 +1,17 @@
-
+const bcrypt = require('bcrypt');
 
 exports.seed = function(knex) {
+
   return knex('users')
     .del()
-    .then(function() {
+    .then(async function() {
+      // Insert hashed passwords into users table
+      const hashedPassword1 = await bcrypt.hash('password123', 10);
+      const hashedPassword2 = await bcrypt.hash('password456', 10);
+
       return knex('users').insert([
-        { username: 'jake', password: 'hashed_password_1' },
-        { username: 'bob', password: 'hashed_password_2' },
+        { username: 'jake', password: hashedPassword1 },
+        { username: 'bob', password: hashedPassword2 },
       ]);
     });
 };
